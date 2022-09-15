f1080077916da4cab646425cee20b5f8b9d17d63
Reza Alaedini <parsa.alaei.pa@gmail.com>
2022-09-15 18:06:03 +0430

    fixed fallback in dynamic page

--- pages/meetups/[meetupId].js
+++ pages/meetups/[meetupId].js
@@ -24,7 +24,7 @@ export const getStaticPaths = async () => {
   client.close();
 
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
